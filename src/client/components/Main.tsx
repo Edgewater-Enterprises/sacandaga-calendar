c2eb9626d6172348954e43e804cbe61baa860446
Chris Leveille <cdleveille@gmail.com>
2025-06-01 12:30:18 -0400

    added self-hosted deploy workflow

--- src/client/components/Main.tsx
+++ src/client/components/Main.tsx
@@ -4,6 +4,7 @@ import FullCalendar from "@fullcalendar/react";
 export const Main = () => {
 	return (
 		<div style={{ padding: "1rem" }}>
+			<h1>Sacandaga Calendar</h1>
 			<FullCalendar plugins={[dayGridPlugin]} initialView="dayGridMonth" />
 		</div>
 	);
