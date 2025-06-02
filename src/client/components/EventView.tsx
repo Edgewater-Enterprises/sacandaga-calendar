348d90e00d7b7d419facaca42dab33789e8b18ef
Chris Leveille <cdleveille@gmail.com>
2025-06-02 14:26:38 -0400

    fix date display issue

--- src/client/components/EventView.tsx
+++ src/client/components/EventView.tsx
@@ -3,18 +3,19 @@ import type { TEvent } from "@shared/types";
 export const EventView = (event: TEvent) => {
 	const { title, start, end, description } = event;
 
-	const startDateObj = new Date(start);
-	startDateObj.setDate(startDateObj.getDate() + 1);
-	const startDate = startDateObj.toLocaleDateString();
-	const endDate = new Date(end).toLocaleDateString();
-
 	return (
 		<div className="event-view">
 			<h1>{title}</h1>
 			<p>
-				{startDate} - {endDate}
+				{displayDate(start)} - {displayDate(end)}
 			</p>
 			{description && <p>{description}</p>}
 		</div>
 	);
+};
+
+const displayDate = (date: string) => {
+	const dateObj = new Date(date);
+	dateObj.setDate(dateObj.getDate() + 1);
+	return dateObj.toLocaleDateString();
 };
